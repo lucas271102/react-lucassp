226e2ac284ba720530d49d65ed033f41435d1beb
lucas271102 <lucas001spe@gmail.com>
2022-12-04 20:07:29 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
-      <ItemListContainer label="Marcas"/>
+      <ItemListContainer label="Comprar"/>
     </div>
   );
 }
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
 import "./ItemListContainer.css"; 
+import mustang from '../assets/img/mustang.jpg';
+import audia3 from '../assets/img/audia3.jpg'
+import bmwm3 from '../assets/img/bmwm3.jpg';
+import civic from '../assets/img/civic.jpg';
 const ItemListContainer = (props) => {
     const {label: elNombreDeMiBoton}= props;
   return (
-    <button className="mi-boton">
+    <div className="cards">
+<img src={mustang} alt=""/>
+<button className="mi-boton">
     
-        {elNombreDeMiBoton}
-    </button>
+  <button>+</button>  {elNombreDeMiBoton}  <button>-</button> 
+</button>
+<img src={audia3} alt=""/>
+<button className="mi-boton">
+    
+    {elNombreDeMiBoton}   
+</button>
+<img src={bmwm3} alt=""/>
+<button className="mi-boton">
+    
+    {elNombreDeMiBoton}   
+</button>
+<img src={civic} alt=""/>
+<button className="mi-boton">
+    
+    {elNombreDeMiBoton}   
+</button>
+<img src={mustang} alt=""/>
+<button className="mi-boton">
+    
+    {elNombreDeMiBoton}   
+</button>
+    </div>
+   
   )
 }
 
